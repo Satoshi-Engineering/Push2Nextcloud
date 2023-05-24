01432fd325c181a926b2474245cb028849218bbd
thespielplatz <informatics@gmx.net>
2023-05-24 11:06:54 +0200

    [TASK] make upload info smaller

--- src/upload.sh
+++ src/upload.sh
@@ -13,5 +13,5 @@ TIMESTAMP=$(date +%Y-%m-%d_%H%M%S)
 UPLOAD_PATH=$NEXTCLOUD_URL$TIMESTAMP-$DEST_FILE
 
 echo "$(get_date) ➡️ Uploading to: $UPLOAD_PATH"
-curl -T $SOURCE_FILE -u "$NEXTCLOUD_USR:$NEXTCLOUD_PWD" $UPLOAD_PATH
+curl --progress-bar -T $SOURCE_FILE -u "$NEXTCLOUD_USR:$NEXTCLOUD_PWD" $UPLOAD_PATH
 echo "$(get_date) ✅ Done"

b0ab2d83497356bec7f91e8e68826e54b2d2314c
Thomas Schagerl <tom@satoshiengineering.com>
2023-09-26 15:07:54 +0200

    [FEATURE] add directory upload projects#649

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,16 +7,35 @@ set -o pipefail
 # set -x # for debug
 
 TAR_WORKING_DIR="/work"
+DIRECTORY_UPLOAD_TAR_FILE="${TAR_WORKING_DIR}/tmp.tar.gz"
 
-[ -z "$SOURCE_FILE" ] && { echo "🚨 ENV: SOURCE_FILE missing!"; exit 2; }
+# check watch mode
+WATCH_MODE=
+if [ ! -z "$SOURCE_FILE" ] && [ ! -z "$SOURCE_DIRECTORY" ]; then
+  echo "🚨 ENV: SOURCE_FILE and SOURCE_DIRECTORY set, set exactly one!"
+  exit 2
+elif [ -z "$SOURCE_FILE" ] && [ -z "$SOURCE_DIRECTORY" ]; then
+  echo "🚨 ENV: SOURCE_FILE and SOURCE_DIRECTORY missing, set exactly one!"
+  exit 2
+elif [ ! -z "$SOURCE_FILE" ]; then
+  WATCH_MODE=File
+else
+  WATCH_MODE=Directory
+fi
 
-[ -z "$DEST_FILE" ] && { echo "🚨 ENV: SOURCE_FILE missing!"; exit 2; }
-[ -z "$DEST_FILE_TAR" ] && { echo "ℹ️  ENV: DEST_FILE_TAR not set. Setting to mode 'false'!"; DEST_FILE_TAR="false"; }
+# check tar of uploaded file, prevent for directory upload (directory is tar anyways)
+if [ $WATCH_MODE == "Directory" ] && [ "$DEST_FILE_TAR" == "true" ]; then
+  DEST_FILE_TAR="false"
+elif [ -z "$DEST_FILE_TAR" ]; then
+  echo "ℹ️  ENV: DEST_FILE_TAR not set. Setting to mode 'false'!"
+  DEST_FILE_TAR="false"
+fi
 
+# check nextcloud config
+[ -z "$DEST_FILE" ] && { echo "🚨 ENV: DEST_FILE missing!"; exit 2; }
 [ -z "$NEXTCLOUD_USR" ] && { echo "🚨 ENV: NEXTCLOUD_USR missing!"; exit 2; }
 [ -z "$NEXTCLOUD_PWD" ] && { echo "🚨 ENV: NEXTCLOUD_PWD missing!"; exit 2; }
 [ -z "$NEXTCLOUD_URL" ] && { echo "🚨 ENV: NEXTCLOUD_URL missing!"; exit 2; }
-
 [ -z "$NEXTCLOUD_UPLOAD_MODE" ] && { echo "ℹ️  ENV: NEXTCLOUD_UPLOAD_MODE not set. Setting to mode 'newfile'!"; NEXTCLOUD_UPLOAD_MODE="newfile"; }
 
 get_date () {
@@ -24,15 +43,16 @@ get_date () {
 }
 
 check_file () {
-  [ ! -e "$SOURCE_FILE" ] && { echo "$(get_date) 🚨 SOURCE_FILE: $SOURCE_FILE is missing or was removed!"; exit 3; }
+  [ $WATCH_MODE == "File" ] && [ ! -e "$SOURCE_FILE" ] && { echo "$(get_date) 🚨 SOURCE_FILE: $SOURCE_FILE is missing or was removed!"; exit 3; }
+  [ $WATCH_MODE == "Directory" ] && [ ! -d "$SOURCE_DIRECTORY" ] && { echo "$(get_date) 🚨 SOURCE_DIRECTORY: $SOURCE_DIRECTORY is missing or was removed!"; exit 3; }
   return 0
 }
 
 startup_checks () {
   # Check if Tar working directory does not exist
-  if [ ! -d "$TAR_WORKING_DIR" ]; then
+  if ([ "$DEST_FILE_TAR" == "true" ] || [ $WATCH_MODE == "Directory" ]) && [ ! -d "$TAR_WORKING_DIR" ]; then
     # Take action if $DIR exists.
-    echo "Creating Working directory in ${TAR_WORKING_DIR}"
+    echo "ℹ️  SETUP: Creating Working directory inside container: ${TAR_WORKING_DIR}"
     mkdir $TAR_WORKING_DIR
   fi
 }
@@ -42,21 +62,33 @@ run_mode_file_change () {
   # Check if file exists
   check_file
 
-  # Monitor File
-  while true; do
+  if [ $WATCH_MODE == "File" ]; then
+    # Monitor File
+    while true; do
       echo "$(get_date) 👀 Watching: $SOURCE_FILE"
       inotifywait $SOURCE_FILE
       echo "$(get_date) ➡️  $SOURCE_FILE has been changed!"
       check_file
 
-      source /app/upload.sh
-  done
+      source /app/upload-file.sh
+    done
+  elif [ $WATCH_MODE == "Directory" ]; then
+    # Monitor Directory
+    while true; do
+      echo "$(get_date) 👀 Watching: $SOURCE_DIRECTORY"
+      inotifywait $SOURCE_DIRECTORY
+      echo "$(get_date) ➡️  $SOURCE_DIRECTORY has been changed!"
+      check_file
+
+      source /app/upload-directory.sh
+    done
+  fi
 }
 
-echo "$(get_date) ⚙️  Mode: Watch File Change"
+startup_checks
+
+echo "$(get_date) ⚙️  Mode: Watch $WATCH_MODE Change"
 echo "$(get_date) ⚙️  Upload Mode: $NEXTCLOUD_UPLOAD_MODE"
 echo "$(get_date) ⚙️  Tar Destination File: $DEST_FILE_TAR"
-
-startup_checks
 
 run_mode_file_change
